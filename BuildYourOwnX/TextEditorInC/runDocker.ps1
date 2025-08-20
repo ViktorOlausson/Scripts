ed2aafc324fc3eecd105a2c3ff2a5195f9f41cc2
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2025-08-20 15:23:39 +0200

    Pull docker image + output color

--- BuildYourOwnX/TextEditorInC/runDocker.ps1
+++ BuildYourOwnX/TextEditorInC/runDocker.ps1
@@ -1,34 +1,54 @@
 try{
-    $docker = docker --version 2>$null
-    if($LASTEXITCODE -eq 0){
-        Write-Output "Docker Engine is installed" #no output if installed?
-    }else {
-        Write-Output "Docekr Engine is NOT installed, please install Docker to run this command"
-        exit 1
-    }
-}catch{
-    Write-Output "Docekr Engine is NOT installed, please install Docker to run this command"
-}
-
-try{
+    Write-Host "Checking if Docker Desktop is installed..." -ForegroundColor Yellow
     $dockerHub = docker login --help 2>$null
     if($LASTEXITCODE -eq 0){
-        Write-Output "Docker Hub CLI is installed"
+        Write-Host "Docker Hub CLI is installed" -ForegroundColor Green
     }else{
-        Write-Output "Docker Hub CLI is NOT installed, please install Docker to run this command"
+        Write-Host "Docker Hub CLI is NOT installed, please install Docker to run this command"
     }
 }catch{
-    Write-Output "Docker Hub CLI is NOT available, please install Docker to run this command"
+    Write-Host "Docker Hub CLI is NOT available, please install Docker to run this command"
 }
 
 try{
-    docker info >$null 2>&1
+    Write-Host "Checking if Docker engine is installed..." -ForegroundColor Yellow
+    $docker = docker --version 2>$null
     if($LASTEXITCODE -eq 0){
-        Write-Output "Docker daemon is running"
-    }else{
-        Write-Output "Docker daemon is not running, please start docker and rerun this script"
+        Write-Host "Docker Engine is installed" -ForegroundColor Green #no output if installed?
+    }else {
+        Write-Host "Docekr Engine is NOT installed, please install Docker to run this command"
         exit 1
     }
 }catch{
-    Write-Output "Docker daemon is not running"
+    Write-Host "Docekr Engine is NOT installed, please install Docker to run this command"
+    exit 1
+}
+
+try{
+    Write-Host "Checking if Docker is running..." -ForegroundColor Yellow
+    docker info >$null 2>&1
+    if($LASTEXITCODE -eq 0){
+        Write-Host "Docker daemon is running" -ForegroundColor Green
+    }else{
+        Write-Host "Docker daemon is not running, please start docker and rerun this script"
+        exit 1
+    }
+}catch{
+    Write-Host "Docker daemon is not running"
+    exit 1
+}
+
+try {
+    Write-Host "Pulling docker image..." -ForegroundColor Yellow
+    docker pull "viktorolausson/texteditorinc" > $null 2>&1
+    if($LASTEXITCODE -eq 0){
+        Write-Host "Pulled docker image" -ForegroundColor Green
+    }else{
+        Write-Host "Failed to pull docker image"
+        exit 1
+    }
+}
+catch {
+    Write-Host "Failed to pull docker image"
+    exit 1
 }
