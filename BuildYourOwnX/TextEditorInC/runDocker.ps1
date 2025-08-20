eb8e6014c3c576b64a231cf1a8aecf21cea0802c
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2025-08-20 15:29:32 +0200

    Update runDocker.ps1

--- BuildYourOwnX/TextEditorInC/runDocker.ps1
+++ BuildYourOwnX/TextEditorInC/runDocker.ps1
@@ -38,6 +38,8 @@ try{
     exit 1
 }
 
+#if docker image already exists, dont
+
 try {
     Write-Host "Pulling docker image..." -ForegroundColor Yellow
     docker pull "viktorolausson/texteditorinc" > $null 2>&1
@@ -52,3 +54,7 @@ catch {
     Write-Host "Failed to pull docker image" -ForegroundColor Red
     exit 1
 }
+
+#ask for file path
+#if it is a folder, ask for a file name to be created and then use that file
+#if it is an file use the parent folder as path and use the file to launch
