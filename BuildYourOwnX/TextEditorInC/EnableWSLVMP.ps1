8c77d511d3f03b25c7548a6673a4ee4d3f93ec75
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2025-08-21 12:15:06 +0200

    improved EnableWSLVMP.ps1

--- BuildYourOwnX/TextEditorInC/EnableWSLVMP.ps1
+++ BuildYourOwnX/TextEditorInC/EnableWSLVMP.ps1
@@ -3,14 +3,28 @@
 function Enable-Feature {
     param ([string]$Name)
     $f = Get-WindowsOptionalFeature -Online -FeatureName $Name
-    if($f.State -ne 'Enabled'){
-        dism.exe /online /enabled-feature /featurename:$name /all /norestart | Out-Null
+
+    if($f.State -eq 'Enabled'){
+        Write-Host "'$Name' already enabled" -ForegroundColor Green
+    }else{
+        try {
+            Write-Host "Enabling '$Name" -ForegroundColor Yellow
+            dism.exe /online /enabled-feature /featurename:$name /all /norestart *> $null
+            if($LASTEXITCODE -eq 0){
+                Write-Host "'$Name' has been enabled" -ForegroundColor Green
+            }else {
+                Write-Host "Something went wrong" -ForegroundColor Red
+            }
+        }
+        catch {
+            Write-Host "error" -ForegroundColor Red
+        }
     }
 }
 
 Enable-Feature Microsoft-Windows-Subsystem-Linux
 Enable-Feature VirtualMachinePlatform
 
-wsl --set-default-version 2
+wsl --set-default-version 2 *> $null
 
-Write-Output "WSL2 prerequisites ensured. If features were just enabled, a reboot may be required."
+Write-Host "WSL2 prerequisites ensured. If features were just enabled, a reboot may be required." -ForegroundColor Green
