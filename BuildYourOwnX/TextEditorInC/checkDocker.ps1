c22ddc08f90bdea2ac842bc2924767e1304800a0
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2025-08-21 11:57:20 +0200

    Check docker and run docekr diffrent files

---
+++ BuildYourOwnX/TextEditorInC/checkDocker.ps1
@@ -0,0 +1,39 @@
+try{
+    Write-Host "Checking if Docker Desktop is installed..." -ForegroundColor Yellow
+    $dockerHub = docker login --help 2>$null
+    if($LASTEXITCODE -eq 0){
+        Write-Host "Docker Hub CLI is installed" -ForegroundColor Green
+    }else{
+        Write-Host "Docker Hub CLI is NOT installed, please install Docker to run this command" -ForegroundColor Red
+    }
+}catch{
+    Write-Host "Docker Hub CLI is NOT available, please install Docker to run this command" -ForegroundColor Red
+}
+
+try{
+    Write-Host "Checking if Docker engine is installed..." -ForegroundColor Yellow
+    $docker = docker --version 2>$null
+    if($LASTEXITCODE -eq 0){
+        Write-Host "Docker Engine is installed" -ForegroundColor Green #no output if installed?
+    }else {
+        Write-Host "Docekr Engine is NOT installed, please install Docker to run this command" -ForegroundColor Red
+        exit 1
+    }
+}catch{
+    Write-Host "Docekr Engine is NOT installed, please install Docker to run this command" -ForegroundColor Red
+    exit 1
+}
+
+try{
+    Write-Host "Checking if Docker is running..." -ForegroundColor Yellow
+    docker info >$null 2>&1
+    if($LASTEXITCODE -eq 0){
+        Write-Host "Docker daemon is running" -ForegroundColor Green
+    }else{
+        Write-Host "Docker daemon is not running, please start docker and rerun this script" -ForegroundColor Red
+        exit 1
+    }
+}catch{
+    Write-Host "Docker daemon is not running" -ForegroundColor Red
+    exit 1
+}
