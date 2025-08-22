4ea31bc235bd3277e0d39b1024bce04be24bf753
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2025-08-22 19:21:24 +0200

    output change

--- checkIsAdmin.ps1
+++ checkIsAdmin.ps1
@@ -3,5 +3,5 @@ $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIde
 if (-not $isAdmin) {
 Write-Host "Please run this script in an elevated PowerShell (Run as Administrator)." -ForegroundColor Red
 }else {
-    Write-Host "In admin" -ForegroundColor Green
+    Write-Host "In admin mode" -ForegroundColor Green
 }
