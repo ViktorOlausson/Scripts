cc2eaa32a34fed8ee088bea72d70f3239d2959ea
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2025-08-22 19:42:35 +0200

    fixed exit code bug
    Fixed exit code bug making check admin script stop everything

--- checkIsAdmin.ps1
+++ checkIsAdmin.ps1
@@ -1,7 +1,9 @@
 $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
 ).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 if (-not $isAdmin) {
-Write-Host "Please run this script in an elevated PowerShell (Run as Administrator)." -ForegroundColor Red
+    Write-Host "Please run this script in an elevated PowerShell (Run as Administrator)." -ForegroundColor Red
+    exit 1
 }else {
     Write-Host "In admin mode" -ForegroundColor Green
+    exit 0
 }
