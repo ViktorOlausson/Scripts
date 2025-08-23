3a3999fd28df4d5ea6071087b08448968c0baae3
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2025-08-23 14:58:45 +0200

    added reusablity

--- pullDocker.ps1
+++ pullDocker.ps1
@@ -1,17 +1,29 @@
 #if docker image already exists, dont pull
 ##add reusablity
+param(
+    [string]$Image = ""
+)
 
-try {
-    Write-Host "Pulling docker image..." -ForegroundColor Yellow
-    docker pull "viktorolausson/texteditorinc" > $null 2>&1
-    if($LASTEXITCODE -eq 0){
-        Write-Host "Pulled docker image" -ForegroundColor Green
-    }else{
+#Write-Host "Checking for docker image" -ForegroundColor Cyan
+docker image inspect $image *> $null
+
+if($LASTEXITCODE -ne 0){
+    Write-Host "Docker image not found exists, pulling latest image" -ForegroundColor Yellow
+    try {
+        Write-Host "Pulling docker image..." -ForegroundColor Yellow
+        docker pull "viktorolausson/texteditorinc" > $null 2>&1
+        if($LASTEXITCODE -eq 0){
+            Write-Host "Pulled docker image" -ForegroundColor Green
+        }else{
+            Write-Host "Failed to pull docker image" -ForegroundColor Red
+            exit 1
+        }
+    }
+    catch {
         Write-Host "Failed to pull docker image" -ForegroundColor Red
         exit 1
     }
-}
-catch {
-    Write-Host "Failed to pull docker image" -ForegroundColor Red
-    exit 1
+}else{
+    Write-Host "Image already exists" -ForegroundColor Green
+    exit 0
 }
